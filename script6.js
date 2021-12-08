5490208a9e632b4f88646c4d57fb6f9bda9d3df2
hb92 <britt.hellemans@gmail.com>
2021-12-08 22:19:19 +0100

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,25 @@
+const pizza = function() {
+    console.log("deeg");
+    console.log("saus");
+    console.log("toppings");
+    console.log("bakken");
+};
+
+pizza();
+pizza();
+pizza();
+
+const sushi = function() {
+    console.log("nori");
+    console.log("rijst");
+    console.log("zalm");
+    console.log("rollen");
+}
+
+sushi();
+
+sushi();
+sushi();
+sushi();
+pizza();
+pizza();
